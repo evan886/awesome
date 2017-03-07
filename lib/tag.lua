00b24b7e45a9e3e332d88815a7235b24c00ef5c5
evan <evan886@gmail.com>
2017-03-07 14:11:17 +0800

    2017年 03月 07日 星期二 14:11:17 CST

---
+++ lib/tag.lua
@@ -0,0 +1,40 @@
+-- Standard awesome library
+local gears = require("gears")
+local awful = require("awful")
+awful.rules = require("awful.rules")
+require("awful.autofocus")
+-- Widget and layout library
+local wibox = require("wibox")
+-- Theme handling library
+local beautiful = require("beautiful")
+-- Notification library
+local naughty = require("naughty")
+local menubar = require("menubar")
+
+-- {{{ Tags
+-- Define a tag table which hold all screen tags.
+tags = {
+    -- www / dev / im / dev / dev / dev / media / util / nautilus
+    names = {'`:T', "1:C", "3:IM", "q:D", "w:D", "e:M", "a:U", "s:M"},
+    layouts = {
+        awful.layout.suit.tile,
+        awful.layout.suit.tile,
+        awful.layout.suit.tile,
+        awful.layout.suit.tile,
+        awful.layout.suit.tile,
+        awful.layout.suit.tile,
+        awful.layout.suit.floating,
+        awful.layout.suit.floating,
+        awful.layout.suit.floating
+    }
+}
+for s = 1, screen.count() do
+    -- Each screen has its own tag table.
+    if s == 1 then
+        tags[s] = awful.tag(tags.names, s, tags.layouts)
+    else -- The second screen
+        tags[s] = awful.tag(tags.names, s, awful.layout.suit.tile)
+    end
+end
+-- }}}
+
---
+++ menu.lua
@@ -0,0 +1,43 @@
+-- Standard awesome library
+local gears = require("gears")
+local awful = require("awful")
+awful.rules = require("awful.rules")
+require("awful.autofocus")
+-- Widget and layout library
+local wibox = require("wibox")
+-- Theme handling library
+local beautiful = require("beautiful")
+-- Notification library
+local naughty = require("naughty")
+local menubar = require("menubar")
+
+require("lib/error")
+require("variables")
+require("lib/wallpaper")
+require("lib/tag")
+
+
+-- {{{ Menu
+-- Create a laucher widget and a main menu
+myawesomemenu = {
+   { "manual", terminal .. " -e man awesome" },
+   { "edit config", editor_cmd .. " " .. awesome.conffile },
+   { "restart", awesome.restart },
+   { "quit", awesome.quit }
+}
+
+mymainmenu = awful.menu({
+    items = {
+        { "awesome", myawesomemenu, beautiful.awesome_icon },
+        { "open terminal", terminal }
+    }
+})
+
+mylauncher = awful.widget.launcher({
+    image = beautiful.awesome_icon,
+    menu = mymainmenu
+})
+
+-- Menubar configuration
+menubar.utils.terminal = terminal -- Set the terminal for applications that require it
+-- }}}
